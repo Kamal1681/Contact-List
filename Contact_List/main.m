2d50b28844241327a394ffb46b857e825b9c48f1
Kamal Maged <kamalmaged@kamals-macbook-pro.local>
2019-03-21 17:14:38 -0400

    Initial Commit

---
+++ Contact_List/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Contact_List
+//
+//  Created by Kamal Maged on 2019-03-21.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
